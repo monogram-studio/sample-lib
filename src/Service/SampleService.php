edc4d0ed73307d52f5fe9be8f36c023df22e07d8
Michal Douša <michal.dousa@monogramstudio.cz>
2023-07-26 14:34:57 +0200

    first commit

---
+++ src/Service/SampleService.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MngLabs\SampleLibrary\Service;
+
+final class SampleService
+{
+    public function greet(): string
+    {
+        return 'Hello!';
+    }
+}
